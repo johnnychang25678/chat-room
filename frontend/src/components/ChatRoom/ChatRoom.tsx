ea1f954e2288136a83d13128101a670de1e727b0
Johnny Chang <zjps3407@gmail.com>
2022-06-17 18:13:34 +0800

    feat: add socket io client

--- frontend/src/components/ChatRoom/ChatRoom.tsx
+++ frontend/src/components/ChatRoom/ChatRoom.tsx
@@ -1,9 +1,80 @@
 import { MessageBox, MessageFrom } from "./MessageBox"
 import { NavBar } from "./Navbar"
 import { OnlineChatter } from "./OnlineChatter"
+import io from "socket.io-client"
+import React, { useEffect, useMemo, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 
+type LocationData = {
+  data: string
+}
+
+// TODO: when component unmount, should close the connection with ws and clear the name list
 
 export const ChatRoom = () => {
+  const [socket, setSocket] = useState<any>(null);
+  const [name, setName] = useState<string>("");
+  const [chatInput, setChatInput] = useState<string>("");
+  const [names, setNames] = useState<string[]>([]);
+
+  const location = useLocation()
+  console.log(location)
+  const isLocationStateExist = location.state !== null
+  console.log(isLocationStateExist)
+  // connect to ws  
+  useEffect(() => {
+    if (isLocationStateExist) {
+      const socket = io("http://localhost:8080")
+      socket.on("reply", (message: any) => {
+        console.log(message)
+      })
+
+      socket.on("names-list", (namesFromServer: string[]) => {
+        console.log(namesFromServer)
+        setNames([...names, ...namesFromServer])
+      })
+
+      setSocket(socket)
+    }
+
+  }, []);
+
+  // get the data passed from router useNavigate  
+  useEffect(() => {
+    if (isLocationStateExist) {
+      const locState = location.state as LocationData
+      console.log(locState.data)
+      setName(locState.data)
+    }
+  }, []);
+
+  function emitName(name: string) {
+    if (name !== "") {
+      socket.emit("names", name)
+    }
+  }
+
+  useMemo(() => {
+    emitName(name)
+  }, [socket, name])
+
+  function handleInputChange(input: string) {
+    setChatInput(input)
+  }
+
+  function handleKeydown(e: React.KeyboardEvent<HTMLElement>) {
+    if (e.key === "Enter") {
+      // sendMessage(chatInput)
+      handleInputChange("")
+    }
+  }
+
+  // function sendMessage(message: string) {
+  //   if (socket !== null) {
+  //     socket.emit("notice", message)
+  //   }
+  // }
+
   function genMessageBoxes(): JSX.Element[] {
     const boxes: JSX.Element[] = []
     for (let i = 0; i < 10; i++) {
@@ -16,32 +87,42 @@ export const ChatRoom = () => {
     return boxes
   }
 
+
+
   return (
     <>
-      <div className="flex-col h-screen">
-        <NavBar />
-        <div className="flex h-[83%]">
-          <div className="w-[30%] max-w-[300px] bg-blue-500 hidden md:block">
-            <OnlineChatter name="Johnny Chang" />
-            <OnlineChatter name="Sarah Pan" />
-          </div>
-
-          <div className="grow flex-col bg-green-200">
-            {/* scrollable wrapper */}
-            <div className="overflow-auto h-[90%] bg-white px-2">
-              {
-                genMessageBoxes()
-              }
+      {name === "" ?
+        <div>
+          Please go to {<Link to="/" className="text-blue-500"> home</Link>} and enter your name to join
+        </div> :
+        <div className="flex-col h-screen">
+          <NavBar />
+          <div className="flex h-[83%]">
+            <div className="w-[30%] max-w-[300px] bg-blue-500 hidden md:block">
+              {names.map(name => <OnlineChatter name={name} />)}
             </div>
-            <div className="flex items-center h-[10%]">
-              <input type="text" placeholder="Say Something..." className="outline outline-1 outline-cyan-500 ml-2 w-4/5 p-1 rounded" />
+
+            <div className="grow flex-col bg-green-200">
+              {/* scrollable wrapper */}
+              <div className="overflow-auto h-[90%] bg-white px-2">
+                {
+                  genMessageBoxes()
+                }
+              </div>
+              <div className="flex items-center h-[10%]">
+                <input
+                  onChange={(e) => handleInputChange(e.target.value)}
+                  onKeyDown={(e) => handleKeydown(e)}
+                  value={chatInput}
+                  type="text"
+                  placeholder="Say Something..."
+                  className="outline outline-1 outline-cyan-500 ml-2 w-4/5 p-1 rounded" />
+              </div>
             </div>
+
           </div>
-
-        </div>
-        <footer className="h-[7%] w-full text-center text-gray-500 bg-slate-400 pt-2 ">Footer</footer>
-      </div>
-
+          <footer className="h-[7%] w-full text-center text-gray-500 bg-slate-400 pt-2 ">Footer</footer>
+        </div>}
     </>
 
   )
--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,18 +1,31 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { useNavigate, Navigate } from "react-router-dom"
+import { ChatRoom } from "./ChatRoom/ChatRoom"
 
 export const HomePage = () => {
-  const navigate = useNavigate()
-  const toChatRoom = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    navigate("/chat-room")
+  const [name, setName] = useState<string>("");
+  function handleChange(input: string) {
+    setName(input)
   }
+
+  const navigate = useNavigate()
+  function handleKeydown(e: React.KeyboardEvent<HTMLElement>) {
+    if (e.key === "Enter") {
+      navigate("/chat-room", { state: { data: name } })
+    }
+  }
+
   return (
     <div className="container flex items-center justify-center h-screen">
       <div className="border-solid border-[1px] p-10 border-gray-400 rounded">
         <p>Please enter your name:</p>
-        <form onSubmit={toChatRoom}>
-          <input type="text" name="name" placeholder="Johnny" className="outline outline-1 outline-cyan-500 mt-3 p-1.5" />
-        </form>
+        <input
+          type="text"
+          onChange={(e) => handleChange(e.target.value)}
+          onKeyDown={handleKeydown}
+          value={name}
+          placeholder="Johnny"
+          className="outline outline-1 outline-cyan-500 mt-3 p-1.5" />
       </div>
     </div>
   )
