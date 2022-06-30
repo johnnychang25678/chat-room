83e5ef0b641633ec541eb23e510c8d8b02111a4b
Johnny Chang <zjps3407@gmail.com>
2022-06-30 14:37:16 +0800

    feat: add nameSet and onEvent chat

--- api/main.go
+++ api/main.go
@@ -9,6 +9,19 @@ import (
 	"github.com/rs/cors"
 )
 
+type User struct {
+	Id   string
+	Name string
+}
+
+var NameSet map[string]User = make(map[string]User)
+var connectedNames []string = make([]string, 0)
+
+// type Message struct {
+// 	From    string `json:"from"`
+// 	Content string `json:"content"`
+// }
+
 func main() {
 	server := socketio.NewServer(nil) // client has to be version 1.x
 	server.OnConnect("/", func(s socketio.Conn) error {
@@ -17,23 +30,21 @@ func main() {
 		return nil
 	})
 
-	connectedNames := make([]string, 0)
 	// add names
 	server.OnEvent("/", "names", func(s socketio.Conn, name string) {
 		fmt.Println("received name: ", name)
-		connectedNames = append(connectedNames, name)
+		_, ok := NameSet[name]
+		if !ok {
+			NameSet[name] = User{Id: s.ID(), Name: name}
+			connectedNames = append(connectedNames, name)
+		}
+
 		server.BroadcastToNamespace("/", "names-list", connectedNames)
 	})
 
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		server.BroadcastToNamespace("/", "reply", "have "+msg)
-		// s.Emit("reply", "have "+msg)
-	})
-
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
+	server.OnEvent("/", "chat", func(s socketio.Conn, msg string) {
+		fmt.Println("chat:", msg)
+		server.BroadcastToNamespace("/", "chat", msg)
 	})
 
 	server.OnEvent("/", "bye", func(s socketio.Conn) string {
@@ -47,8 +58,23 @@ func main() {
 		fmt.Println("meet error:", e)
 	})
 
+	// this will trigger when user close the window
+	// TODO: graceful close ws??
 	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("closed", reason)
+		fmt.Println("disconneted by client, reason:", reason)
+		// remove from NameSet
+		for name, user := range NameSet {
+			if user.Id == s.ID() {
+				delete(NameSet, name)
+			}
+		}
+		// remove from connectedNames and broadcast it
+		newConnectedNames := make([]string, 0)
+		for name := range NameSet {
+			newConnectedNames = append(newConnectedNames, name)
+		}
+		connectedNames = newConnectedNames
+		server.BroadcastToNamespace("/", "names-list", connectedNames)
 	})
 
 	go server.Serve()
